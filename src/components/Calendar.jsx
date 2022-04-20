fcb5bc10be038439b0dd3de8caa586c9e9065597
leejungho9 <9wjdgh9@naver.com>
2022-04-20 17:08:37 +0900

    캘린더 Pagination 수정

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import "./Calendar.css";
-import Slider from "./Slider";
+import {
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Calendar() {
   const [isActive, setisActive] = useState("0");
 
@@ -58,11 +63,137 @@ export default function Calendar() {
               2017
             </span>
           </div>
-          <input type="text" placeholder="Search" className="search"></input>
+          <div class="searchArea">
+            <input type="text" placeholder="Search" className="search"></input>
+            <button class="searchBtn">
+              <img src="./images/search.png" alt="검색" class="searchImg"></img>
+            </button>
+          </div>
         </div>
 
         <div className="inner">
-          <Slider />
+          <div className="box">
+            <img
+              alt="캘린더"
+              className="calendarImage"
+              src="../images/calendar_02.png"
+            />
+            <div className="info">
+              <span>2022</span>
+              <span>삼성생명 VIP</span>
+            </div>
+          </div>
+
+          <div className="box">
+            <img
+              alt="캘린더"
+              className="calendarImage"
+              src="../images/calendar_02.png"
+            />
+            <div className="info">
+              <span>2022</span>
+              <span>삼성생명 VIP</span>
+            </div>
+          </div>
+
+          <div className="box">
+            <img
+              alt="캘린더"
+              className="calendarImage"
+              src="../images/calendar_02.png"
+            />
+            <div className="info">
+              <span>2022</span>
+              <span>삼성생명 VIP</span>
+            </div>
+          </div>
+
+          <div className="box">
+            <img
+              alt="캘린더"
+              className="calendarImage"
+              src="../images/calendar_02.png"
+            />
+            <div className="info">
+              <span>2022</span>
+              <span>삼성생명 VIP</span>
+            </div>
+          </div>
+
+          <div className="box">
+            <img
+              alt="캘린더"
+              className="calendarImage"
+              src="../images/calendar_02.png"
+            />
+            <div className="info">
+              <span>2022</span>
+              <span>삼성생명 VIP</span>
+            </div>
+          </div>
+
+          <div className="box">
+            <img
+              alt="캘린더"
+              className="calendarImage"
+              src="../images/calendar_02.png"
+            />
+            <div className="info">
+              <span>2022</span>
+              <span>삼성생명 VIP</span>
+            </div>
+          </div>
+        </div>
+        <div className="pageArea">
+          <nav className="toolbox">
+            <ul className="pagination">
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  <FontAwesomeIcon icon={faAngleDoubleLeft} />
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  1
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  2
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  3
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  4
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  5
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  6
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  7
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link page-link-btn" href="#">
+                  <FontAwesomeIcon icon={faAngleDoubleRight} />
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </section>
